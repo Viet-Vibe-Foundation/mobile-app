cad3a7f09e0ca9953135ae094bad4530a0b81ab7
kuro2904 <trung2001sgp@gmail.com>
2025-03-10 17:56:59 +0700

    Add datetime util

--- vvf_mobile/src/utils/dateTimeUtil.ts
+++ vvf_mobile/src/utils/dateTimeUtil.ts
@@ -1,4 +1,4 @@
 import dayjs from 'dayjs';
 
 export const dateToString = (date: Date | null) =>
-  date != null ? dayjs(date).format('DD/MM/YYYY') : '';
+  date != null ? dayjs(date).format('DD/MM/YYYY h:mm a') : '';
