6779c5d6d858a5aa622c994ff105863a7d1696f8
kuro2904 <trung2001sgp@gmail.com>
2025-03-11 23:53:24 +0700

    Add Post Screen, Show post content, update IconTextComponent

---
+++ vvf_mobile/src/ui/main/home/components/IconTextComponent.tsx
@@ -0,0 +1,48 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import {MaterialIconName} from '@types/materialType';
+
+interface Props {
+  icon: MaterialIconName;
+  text: string;
+  iconColor?: string;
+  textColor?: string;
+  textSize?: number;
+  iconSize?: number;
+}
+
+const IconTextComponent: React.FC<Props> = ({
+  icon,
+  text,
+  iconColor = 'black',
+  textColor = 'black',
+  textSize = 15,
+  iconSize = 20,
+}) => {
+  return (
+    <View style={styles.container}>
+      <MaterialIcons name={icon} size={iconSize} color={iconColor} />
+      <Text
+        style={[styles.text, {color: textColor, fontSize: textSize}]}
+        numberOfLines={1}
+        ellipsizeMode="tail">
+        {text}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+  text: {
+    fontSize: 15,
+    color: 'black',
+  },
+});
+
+export default IconTextComponent;
--- vvf_mobile/src/ui/main/post/PostScreen.tsx
+++ vvf_mobile/src/ui/main/post/PostScreen.tsx
@@ -1,12 +1,63 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import WebView from 'react-native-webview';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import axiosInstance from 'src/services/apis/axios';
+import {Post} from 'src/data/post';
+import ResponseDTO from 'src/data/responseDTO';
+import HtmlComponent from 'src/ui/components/HtmlComponent';
+
+type PostScreenParams = {
+  postId: string;
+};
 
 const PostScreen = () => {
+  const route = useRoute<RouteProp<{params: PostScreenParams}, 'params'>>();
+  const {postId} = route.params;
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [postContent, setPostContent] = useState<Post | null>(null);
+
+  useEffect(() => {
+    fetchPostContent();
+  }, []);
+
+  const fetchPostContent = async () => {
+    try {
+      setLoading(true);
+      const res = await axiosInstance.get<ResponseDTO<Post>>(
+        `/posts/${postId}`,
+      );
+      if (res.data && res.data.success && res.data.data) {
+        setPostContent(res.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  console.log(postId);
+
   return (
-    <View>
-      <Text>PostScreen</Text>
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size={'large'} />
+      ) : (
+        <>
+          {postContent?.content ? (
+            <HtmlComponent html={postContent.content} />
+          ) : null}
+        </>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default PostScreen;
