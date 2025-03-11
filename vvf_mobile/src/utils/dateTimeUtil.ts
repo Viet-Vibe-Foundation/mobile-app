40101584142a217ced5747c91416af2f910517b2
kuro2904 <trung2001sgp@gmail.com>
2025-03-11 23:46:50 +0700

    Add convert timeToDate, calculateMinute in DateTimeUtil

--- vvf_mobile/src/utils/dateTimeUtil.ts
+++ vvf_mobile/src/utils/dateTimeUtil.ts
@@ -2,3 +2,22 @@ import dayjs from 'dayjs';
 
 export const dateToString = (date: Date | null) =>
   date != null ? dayjs(date).format('DD/MM/YYYY h:mm a') : '';
+
+const timeToDate = (time: string) => {
+  const [hour, minute] = time.split(':').map(Number);
+  return dayjs().hour(hour).minute(minute).second(0);
+};
+
+export const calculateMinute = (to: string, from: string) => {
+  const toDate = timeToDate(to);
+  const fromDate = timeToDate(from);
+
+  if (!toDate.isValid() || !fromDate.isValid()) {
+    console.error('Invalid date format');
+    return 'N/A';
+  }
+
+  const result = fromDate.diff(toDate, 'minute');
+  console.log(`Difference in minutes: ${result}`);
+  return result;
+};
