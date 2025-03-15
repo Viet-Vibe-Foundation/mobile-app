f5ba7c59ac6e53244b319c11915466faa67f60f7
kuro2904 <trung2001sgp@gmail.com>
2025-03-15 09:28:22 +0700

    Add User Info card

--- vvf_mobile/src/ui/main/setting/components/UserInfoComponent.tsx
+++ vvf_mobile/src/ui/main/setting/components/UserInfoComponent.tsx
@@ -1,20 +1,63 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
+import CircularAvatar from 'src/ui/components/CircularAvatar';
+import MaterialIcon from '@react-native-vector-icons/material-icons';
 
 interface Props {
   name: string;
   email: string;
-  phone: string;
-  address: string;
-  imageUrl: string;
+  image: string;
 }
 
-const UserInfoComponent = ({name, email, phone, address, imageUrl}: Props) => {
+const UserInfoComponent = ({name, email, image}: Props) => {
+  const handleAddImage = () => {};
+
   return (
-    <View>
-      <Text>UserInfoComponent</Text>
+    <View style={styles.container}>
+      <View>
+        <CircularAvatar imageUrl={image} />
+        <TouchableOpacity
+          onPress={handleAddImage}
+          style={styles.addImageButton}>
+          <MaterialIcon name="add" size={24} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>{name}</Text>
+        <Text>{email}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    borderRadius: 20,
+    width: '100%',
+    padding: 10,
+    backgroundColor: 'white',
+    elevation: 5,
+    shadowOpacity: 0.1,
+  },
+  infoContainer: {
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  addImageButton: {
+    position: 'absolute',
+    bottom: -5,
+    right: -5,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    borderWidth: 0.5,
+  },
+});
 
 export default UserInfoComponent;
