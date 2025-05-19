afb0d52eef696d0770095fbe7158a991afaa5fac
kuro2904 <trung2001sgp@gmail.com>
2025-05-20 01:27:59 +0700

    Add services

---
+++ vvf_mobile/src/services/postService.ts
@@ -0,0 +1,23 @@
+import ResponseDTO from '@data/responseDTO';
+import axiosInstance from './apis/axios';
+import {Post} from '@data/post';
+import {Alert} from 'react-native';
+
+export const getPosts = async (pageNum: number = 1) => {
+  try {
+    const res = await axiosInstance.get<ResponseDTO<Post[]>>(
+      `/posts/get?isPublished=true&pageNum=${pageNum}`,
+    );
+    const newPosts = res.data.data || [];
+    if (newPosts.length === 0) {
+      return;
+    }
+    return {
+      data: newPosts,
+      total: res.data.total,
+    };
+  } catch (error: any) {
+    console.error(error);
+    Alert.alert('Error', 'Failed to load posts. Please try again.');
+  }
+};
